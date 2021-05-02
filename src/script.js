4bfc03a4ac4da10cd5ae4ba6f073571b41cab85c
ejunior95 <e.junior95@gmail.com>
2021-05-01 21:12:36 -0300

    [UPDATE]-> Instalando dependencias e utilizando classes do THREE

---
+++ src/script.js
@@ -0,0 +1,34 @@
+import './style.css'
+import * as THREE from 'three'
+// import * as dat from 'dat.gui'
+
+// Scene
+const scene = new THREE.Scene();
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh)
+
+// Camera
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3
+
+scene.add(camera)
+
+// Renderer
+const canvas =  document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
